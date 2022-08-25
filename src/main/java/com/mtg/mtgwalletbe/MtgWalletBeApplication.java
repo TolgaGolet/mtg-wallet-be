592c8fc14238c7f05a22539edb9216955f08c0b6
tolga.golet <tolga.golet@metasis.com.tr>
2022-08-25 22:24:20 +0300

    initial commit

---
+++ src/main/java/com/mtg/mtgwalletbe/MtgWalletBeApplication.java
@@ -0,0 +1,13 @@
+package com.mtg.mtgwalletbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MtgWalletBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MtgWalletBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtg/mtgwalletbe/MtgWalletBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtg.mtgwalletbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtgWalletBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
