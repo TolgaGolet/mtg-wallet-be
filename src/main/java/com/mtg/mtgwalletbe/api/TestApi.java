b8d2ce335fb9ef0a337d38fa59357d89653ceccd
tolgagolet <m.tolgaglt@gmail.com>
2024-06-12 23:13:12 +0300

    removed dev profile

--- src/main/java/com/mtg/mtgwalletbe/api/TestApi.java
+++ src/main/java/com/mtg/mtgwalletbe/api/TestApi.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Profile({"disabled-security", "dev", "test"})
+@Profile({"disabled-security", "test"})
 @RestController
 @RequestMapping("/test")
 @RequiredArgsConstructor
